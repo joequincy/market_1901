7548d6858785b93fa0ace7991a93fe00aa97f9cc
Jon Peterson <jon@pnwebs.com>
2019-02-20 09:34:00 -0700

    Complete Iteration 2.

--- test/market_test.rb
+++ test/market_test.rb
@@ -4,5 +4,56 @@ require './lib/market'
 require './lib/vendor'
 
 class MarketTest < Minitest::Test
+  def setup
+    @market = Market.new("South Pearl Street Farmers Market")
 
+    @vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor_1.stock("Peaches", 35)
+    @vendor_1.stock("Tomatoes", 7)
+
+    @vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor_2.stock("Banana Nice Cream", 50)
+    @vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+
+    @vendor_3 = Vendor.new("Palisade Peach Shack")
+    @vendor_3.stock("Peaches", 65)
+  end
+
+  def test_it_exists
+    assert_instance_of Market, @market
+  end
+
+  def test_attributes_initialize_correctly
+    assert_equal "South Pearl Street Farmers Market", @market.name
+    assert_equal [], @market.vendors
+  end
+
+  def test_market_can_add_vendors
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
+    assert_equal [@vendor_1, @vendor_2, @vendor_3], @market.vendors
+  end
+
+  def test_market_does_not_duplicate_vendors
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_1)
+    assert_equal [@vendor_1], @market.vendors
+  end
+
+  def test_market_returns_names_of_vendors
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
+    expected = ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
+    assert_equal expected, @market.vendor_names
+  end
+
+  def test_market_returns_list_of_vendors_that_sell_an_item
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
+    assert_equal [@vendor_1, @vendor_3], @market.vendors_that_sell("Peaches")
+    assert_equal [@vendor_2], @market.vendors_that_sell("Banana Nice Cream")
+  end
 end
