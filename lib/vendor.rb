45597cb89c0851dfae9fce4b3b72cd38d377893e
Jon Peterson <jon@pnwebs.com>
2019-02-20 09:16:41 -0700

    Complete iteration 1.

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -4,7 +4,22 @@ class Vendor
 
   def initialize(name)
     @name = name
-    @inventory = # you decide what goes here...
+    @inventory = {}# you decide what goes here...
   end
 
+  def check_stock(item)
+    if @inventory.has_key?(item)
+      @inventory.find{|name,details| name = item}[1]
+    else
+      0
+    end
+  end
+
+  def stock(item, quantity)
+    if @inventory.has_key?(item)
+      @inventory[item] += quantity
+    else
+      @inventory[item] = quantity
+    end
+  end
 end
--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -3,19 +3,36 @@ require 'minitest/pride'
 require './lib/vendor'
 
 class VendorTest < Minitest::Test
+  def setup
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
   def test_it_exists
-    vendor = Vendor.new("Rocky Mountain Fresh")
-    assert_instance_of Vendor, vendor
+    assert_instance_of Vendor, @vendor
   end
 
   def test_it_has_a_name
-    vendor = Vendor.new("Rocky Mountain Fresh")
-    assert_equal "Rocky Mountain Fresh", vendor.name
+    assert_equal "Rocky Mountain Fresh", @vendor.name
   end
 
   def test_inventory_starts_as_empty_hash
-    vendor = Vendor.new("Rocky Mountain Fresh")
-    assert_equal ({}), vendor.inventory
+    assert_equal ({}), @vendor.inventory
   end
 
+  def test_it_can_check_stock
+    assert_equal 0, @vendor.check_stock("Peaches")
+  end
+
+  def test_it_can_add_stock
+    @vendor.stock("Peaches", 30)
+    assert_equal 30, @vendor.check_stock("Peaches")
+  end
+
+  def test_inventory_accumulates
+    expected = {"Peaches" => 55, "Tomatoes" => 12}
+    @vendor.stock("Peaches", 30)
+    @vendor.stock("Peaches", 25)
+    @vendor.stock("Tomatoes", 12)
+    assert_equal expected, @vendor.inventory
+  end
 end
